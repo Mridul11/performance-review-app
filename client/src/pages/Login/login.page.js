446a5c9a7e8060642b2342a69569eed3f3ab0c68
Mridul11 <mridulmishra2991@gmail.com>
2021-06-19 09:13:55 +0530

    cleanup: removed the backslash from README

--- client/src/pages/Login/login.page.js
+++ client/src/pages/Login/login.page.js
@@ -1,6 +1,5 @@
 import { useState, useContext } from "react";
 import LoginComponent from '../../components/Login/login.component';
-import { ContextStore } from "../../context/ContextStore";
 
 const LoginPage = () => {
     const [email, emailSet] = useState("");
